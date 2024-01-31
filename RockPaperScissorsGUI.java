14a98e7143b15262cf8cbb9d344951a64fed83f0
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-01-31 23:21:06 +0300

    Add files via upload

---
+++ RockPaperScissorsGUI.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+
+//frontend
+public class RockPaperScissorsGUI extends JFrame {
+    public RockPaperScissorsGUI(){
+        //invoke jframe constructor and add title to the GUI
+    }
+}
