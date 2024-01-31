e5bcc2f9efd4e3c263b93153d8c68839bd1cd4c8
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-01-31 23:30:05 +0300

    Add files via upload

--- App.java
+++ App.java
@@ -1,2 +1,16 @@
+import javax.swing.*;
+
 public class App {
+    public static void main(String[]args){
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+              //instantiate a rockpaperscissor gui
+              RockPaperScissorsGUI rockPaperScissorsGUI=new RockPaperScissorsGUI();
+
+              //display the gui
+                rockPaperScissorsGUI.setVisible(true);
+            }
+        });
+    }
 }
--- RockPaperScissorsGUI.java
+++ RockPaperScissorsGUI.java
@@ -4,5 +4,13 @@ import javax.swing.*;
 public class RockPaperScissorsGUI extends JFrame {
     public RockPaperScissorsGUI(){
         //invoke jframe constructor and add title to the GUI
+        super("Rock Pper Scissors");
+        //set the size of the GUI
+        setSize(450, 574);
+
+        // set layout to null to disable layout management,so we can use absolute positioning for the elements
+        setLayout(null);
+        //load GUI In the centre of the screen every time we run the application
+        setLocationRelativeTo(null);
     }
 }
