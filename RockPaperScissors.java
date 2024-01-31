f56b204066b3ca849d6d4a24c5b5d0e5e4afaf96
Anne Apiyo Opiyo <93118744+1287Apiyo@users.noreply.github.com>
2024-02-01 00:42:37 +0300

    Add files via upload

--- RockPaperScissors.java
+++ RockPaperScissors.java
@@ -1,2 +1,54 @@
+//backend
+
+import java.util.Random;
+
 public class RockPaperScissors {
+//all the choices a computer can choose
+    private static String[] computerChoices ={"Rock","Paper","Scissors"};
+
+    private String computerChoice;
+
+    ///store the scores so that we can retrieve the values and display it to the frontend
+    private int computerScore, playerScore;
+
+    //used to generate  a random number to randomly choose an option for the computer
+    private Random random;
+    //constructor to instantiate random object
+    public RockPaperScissors(){
+        random=new Random();
+    }
+//call this method to begin playing the game
+    public String playRockPaperScissor(String playerChoice){
+      //generate comp choice
+       computerChoice=computerChoices[random.nextInt(computerChoices.length)];
+
+       //will contain the returning message
+                String result;
+        //evaluate the winner
+        if (computerChoice.equals("Rock")){
+            if (playerChoice.equals("Paper")){
+                result="Player wins";
+                playerScore++;
+            } else if (playerChoice.equals("Scissors")) {
+                result="Computer wins";
+                computerScore++;
+                
+            }else {
+                result="Draw";
+            }
+        }else if (computerChoice.equals("Paper")) {
+            if (playerChoice.equals("Scissors")) {
+                result = "Player wins";
+                playerScore++;
+            } else if (playerChoice.equals("Rock")) {
+                result = "Computer wins";
+                computerScore++;
+
+            } else {
+                result = "Draw";
+            }
+        }else {
+            //computer chooses scissors
+        }
+    }
 }
